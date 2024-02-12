4ac0d168374a87a0cddf3bdf270201ab8cec79ca
Anders Miller <anders.miller@gmail.com>
2024-02-12 12:52:48 +0100

    Update environment variable names

--- interesting/settings.py
+++ interesting/settings.py
@@ -100,7 +100,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET')
 }
 
